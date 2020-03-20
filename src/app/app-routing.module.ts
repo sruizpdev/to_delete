2692e72a532718ba386109160acf9a664c0001c5
Sergio Ruiz <sergiotto@outlook.com>
2020-03-20 10:01:56 +0100

    primeros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+ 
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
